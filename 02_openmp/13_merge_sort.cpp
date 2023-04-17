cdfa7a0568e9a508a15b1c8801201423cfa9fd07
sinotca529-wsl <52117877+sinotca529@users.noreply.github.com>
2023-04-17 14:28:26 +0900

    impl 13

--- 02_openmp/13_merge_sort.cpp
+++ 02_openmp/13_merge_sort.cpp
@@ -6,7 +6,7 @@ void merge(std::vector<int>& vec, int begin, int mid, int end) {
   std::vector<int> tmp(end-begin+1);
   int left = begin;
   int right = mid+1;
-  for (int i=0; i<tmp.size(); i++) { 
+  for (int i=0; i<tmp.size(); i++) {
     if (left > mid)
       tmp[i] = vec[right++];
     else if (right > end)
@@ -14,17 +14,20 @@ void merge(std::vector<int>& vec, int begin, int mid, int end) {
     else if (vec[left] <= vec[right])
       tmp[i] = vec[left++];
     else
-      tmp[i] = vec[right++]; 
+      tmp[i] = vec[right++];
   }
-  for (int i=0; i<tmp.size(); i++) 
+  for (int i=0; i<tmp.size(); i++)
     vec[begin++] = tmp[i];
 }
 
 void merge_sort(std::vector<int>& vec, int begin, int end) {
   if(begin < end) {
     int mid = (begin + end) / 2;
+#pragma omp task shared(vec)
     merge_sort(vec, begin, mid);
+#pragma omp task shared(vec)
     merge_sort(vec, mid+1, end);
+#pragma omp taskwait
     merge(vec, begin, mid, end);
   }
 }
@@ -37,6 +40,8 @@ int main() {
     printf("%d ",vec[i]);
   }
   printf("\n");
+#pragma omp parallel
+#pragma omp single
   merge_sort(vec, 0, n-1);
   for (int i=0; i<n; i++) {
     printf("%d ",vec[i]);
